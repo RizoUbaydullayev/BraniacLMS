13291ca3760708337bfbc163b79926f14ffe37b1
rizo <rizoubaydullayev17@gmail.com>
2023-01-10 02:30:52 +0500

    Lesson_3 Urls in templates

--- config/settings.py
+++ config/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "mainapp.context_processor.exemple.simple_context_processor",
+                "mainapp.context_processors.examply.simple_comtext_processor",
             ],
         },
     },
