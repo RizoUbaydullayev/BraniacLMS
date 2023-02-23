cb8a74eddfe8cbd2f6098cc931577fc68634fe73
rizo <rizoubaydullayev17@gmail.com>
2023-02-23 06:57:41 +0500

    Lesson_4 Display model's data

--- mainapp/models.py
+++ mainapp/models.py
@@ -8,24 +8,6 @@ class News(models.Model):
     body_as_markdown = models.BooleanField(default=False, verbose_name="As markdown")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Created", editable=False)
     updated = models.DateTimeField(auto_now=True, verbose_name="Edited", editable=False)
-    deleted = models.BooleanField(default=True)
-
-    def __str__(self):
-        return f"{self.pk} {self.title}"
-
-    def delete(self, *args):
-        self.deleted = True
-        self.save()
-
-
-class Courses(models.Model):
-    name = models.CharField(max_length=256, verbose_name="Name")
-    description = models.TextField(verbose_name="Description", blank=True, null=True)
-    description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
-    cost = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Cost", default=0)
-    cover = models.CharField(max_length=25, default="no_image.svg", verbose_name="Cover")
-    created = models.DateTimeField(auto_now_add=True, verbose_name="Created")
-    updated = models.DateTimeField(auto_now=True, verbose_name="Edited")
     deleted = models.BooleanField(default=False)
 
     def __str__(self) -> str:
@@ -36,16 +18,39 @@ class Courses(models.Model):
         self.save()
 
 
-class Lesson(models.Model):
-    course = models.ForeignKey(Courses, on_delete=models.CASCADE)
-    num = models.PositiveBigIntegerField(verbose_name="Lesson number")
-    title = models.CharField(max_length=256, verbose_name="Title")
+class CoursesManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted=False)
+
+
+class Courses(models.Model):
+    objects = CoursesManager()
+
+    name = models.CharField(max_length=256, verbose_name="Name")
     description = models.TextField(verbose_name="Description", blank=True, null=True)
     description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
     cost = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Cost", default=0)
     cover = models.CharField(max_length=25, default="no_image.svg", verbose_name="Cover")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Created")
     updated = models.DateTimeField(auto_now=True, verbose_name="Edited")
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"{self.pk} {self.name}"
+
+    def delete(self, *args):
+        self.deleted = True
+        self.save()
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Courses, on_delete=models.CASCADE)
+    num = models.PositiveIntegerField(verbose_name="Lesson number")
+    title = models.CharField(max_length=256, verbose_name="Name")
+    description = models.TextField(verbose_name="Description", blank=True, null=True)
+    description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created", editable=False)
+    updated = models.DateTimeField(auto_now=True, verbose_name="Edited", editable=False)
     deleted = models.BooleanField(default=False)
 
     def __str__(self) -> str:
